f54beb97d751edf19c458d9903b01be847262798
Vladan Krstic <6428373+krvladan@users.noreply.github.com>
2020-07-06 23:28:03 +0200

    Require authorization

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,35 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
+// import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+// import Axios from 'axios'
+// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const certificate = `-----BEGIN CERTIFICATE-----
+MIIDBTCCAe2gAwIBAgIJekR2uIYUGmKLMA0GCSqGSIb3DQEBCwUAMCAxHjAcBgNV
+BAMTFWtydmxhZGFuLmV1LmF1dGgwLmNvbTAeFw0yMDA3MDExOTIzMTFaFw0zNDAz
+MTAxOTIzMTFaMCAxHjAcBgNVBAMTFWtydmxhZGFuLmV1LmF1dGgwLmNvbTCCASIw
+DQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMGC1IQ2rORIj71HOOvumLAdz6qa
+eW0ypTiAVUxh4GOt1NmeQzAc3UBw/VfgX1iq5dl2zkkkPRKXKgqMt2UugARjRFTl
+ZxtXls2+bB7xf4s/o1I4xIkWZAnVE4NzRsho4+Ca+dpNzqW8b+naoN/sHCTFN/u6
+C7BfgA/sJZ5SUG2da0IiceUSV4+AYPi3pjFYXd7uxGH1Ka1heMgp7A32rO5Jdxwm
+XgeYfq3lBCw3mlEm6wf5bmm+A4IbkhktkZWnI/DJKjcZoOwgmRFObVxUWrsctvt4
+wqdnqGy6Gk9Bpldge+aFXT9ZmrMLhJD2qxkMH+s4zbZzDzn/ymwxRcxYxzcCAwEA
+AaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUkg4AIdbLSvWyaE69iZ1S
+8EfwUTAwDgYDVR0PAQH/BAQDAgKEMA0GCSqGSIb3DQEBCwUAA4IBAQCJm4u765kN
+kGGvK1yx2RI1Qxg/q3id4zsKBFULtNKthNGdJroHvdqrxZkfUzZrkatTwMYey87S
+DOtpIT6CkTdMx2Z5aHkpMPcJzIQqUoa+E5RdoD29hC6yegDZX3OFn2I0gEWGNMs9
+yYSwj4SVQVQN0Uia+iDW40uz/RNuMz2HGdt0pC6r0OlGNJ67D/0sDV58D0ouueAv
++O+x90xYg4DQAUTQqa0ycA8Um0rBLJC+8sO/LVLwwlHRJcX9aE3kAu/yMb9C34fB
+L5i6Bxcy7NPOREHzmpwCS1hqIuLMmmvPb6Fhhfg5BtWj4a2GjlxojS0ZUgDcbVSG
+XkFJBEB3zcXp
+-----END CERTIFICATE-----`
+
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -56,12 +73,16 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  
+  return verify(token, certificate, { algorithms: ['RS256']}) as JwtPayload
+  
+
+  // return undefined
 }
 
 function getToken(authHeader: string): string {
@@ -75,3 +96,4 @@ function getToken(authHeader: string): string {
 
   return token
 }
+
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,41 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { TodoItem } from '../../models/TodoItem'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event', JSON.stringify(event))
+
   // TODO: Get all TODO items for a current user
+
+  const todoItems: Array<TodoItem> = [
+      {
+        userId: "1",
+        todoId: "123",
+        createdAt: "2019-07-27T20:01:45.424Z",
+        name: "Buy milk",
+        dueDate: "2019-07-29T20:01:45.424Z",
+        done: false,
+        attachmentUrl: "http://example.com/image.png"
+      },
+      {
+        userId: "1",
+        todoId: "456",
+        createdAt: "2019-07-27T20:01:45.424Z",
+        name: "Send a letter",
+        dueDate: "2019-07-29T20:01:45.424Z",
+        done: true,
+        attachmentUrl: "http://example.com/image.png"
+      },
+    ]
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: todoItems
+    })
+  }
 }
