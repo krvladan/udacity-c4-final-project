e7dc5838f2397453e343baa647874c906b3d6ee6
Vladan Krstic <6428373+krvladan@users.noreply.github.com>
2020-07-07 18:55:12 +0200

    Create a todo item and write it to database

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -2,12 +2,61 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-// import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { TodoItem } from '../../models/TodoItem'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+import * as uuid from 'uuid'
+
+import * as AWS from 'aws-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+
+const docClient: DocumentClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+const logger = createLogger('createTodo')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log('Processing event', event)
-  // const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  logger.info('Processing event', event)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const userId = getUserId(event)
+
+  const newItem = await createTodo(newTodo, userId)
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
+}
+
+async function createTodo(
+  createTodoRequest: CreateTodoRequest,
+  userId: string
+): Promise<TodoItem> {
+
+  const itemId = uuid.v4()
+
+  const newItem: TodoItem = {
+    todoId: itemId,
+    userId: userId,
+    name: createTodoRequest.name,
+    dueDate: createTodoRequest.dueDate,
+    done: false,
+    createdAt: new Date().toISOString()
+  }
+  
+  logger.info('Put item', newItem)
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newItem
+  }).promise()
+
+  return newItem
 }
